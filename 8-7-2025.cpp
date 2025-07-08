3cc36b1c49d673374bb777e268ea915f30144e35
Shivam Shende <140915731+ShivamShende2608@users.noreply.github.com>
2025-07-08 09:07:57 +0530

    8-7-2025.cpp

---
+++ 8-7-2025.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        
+        int previous1=nums[0];
+        int previous2 = 0;
+        int current=0;
+
+        for(int i = 1;  i < n; i++){
+            current=max(previous1, nums[i]+previous2);
+            previous2=previous1;
+            previous1=current;
+        }
+        return current;
+        
+    }
+};
