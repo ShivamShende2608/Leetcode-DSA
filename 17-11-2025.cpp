2aed30a00a2b86dd8b2dfea2fa0bfae8ac68ac07
Shivam Shende <140915731+ShivamShende2608@users.noreply.github.com>
2025-11-17 11:41:42 +0530

    17-11-2025.cpp

---
+++ 17-11-2025.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n=word1.size();
+        int m=word2.size();
+
+        vector<vector<int>>dp(n+1 ,vector<int>(m+1,0));
+
+        for(int j=0; j< m+1;j++){
+            dp[0][j]=j;
+        }
+
+        for(int i=0; i< n+1;i++){
+            dp[i][0]=i;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    int insertCost=dp[i][j-1];
+                    int deleteCost=dp[i-1][j];
+                    int replaceCost=dp[i-1][j-1];
+
+                    dp[i][j]= 1 + min({insertCost, deleteCost, replaceCost});
+                }
+            }
+        }
+  return dp[n][m];
+
+        
+    }
+};
