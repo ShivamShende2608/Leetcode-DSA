4646259230e8aad26d9a148418a3233950d3983e
Shivam Shende <140915731+ShivamShende2608@users.noreply.github.com>
2026-01-17 09:30:01 +0530

    17-01-26.cpp

---
+++ 17-01-26.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+
+        for(int i=0; i<n;i++){
+
+        while(nums[i]!=nums[nums[i]-1]){
+            swap(nums[i],nums[nums[i]-1]);  
+        }
+        }
+
+        vector<int>result;
+        for(int i=0;i<n;i++){
+
+            if(nums[i]!=i+1){
+            result.push_back(i+1);
+            }
+        }
+        return result;
+    }
+};
